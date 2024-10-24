54f37daf37362f0b6add415e58bf226a601a2281
Tom Sikora <tom.sikora03@gmail.com>
2024-10-24 15:18:00 +0200

    feat: change main route

--- src/Controller/CapsuleController.php
+++ src/Controller/CapsuleController.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-#[Route('/capsule')]
+#[Route('/')]
 class CapsuleController extends AbstractController
 {
 
